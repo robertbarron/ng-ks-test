752704cd52a803c52135292a9312cf6e4223e02f
Roberto Barron <=>
2014-09-22 00:49:20 -0700

    cleaning up environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -550,8 +550,8 @@ module.exports = function ( grunt ) {
    * The `build` task gets your app ready to run for development and testing.
    */
   grunt.registerTask( 'build', [
-    'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'sass:build',
-    'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
+    'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'concat:build_css',
+    'sass:build', 'copy:build_app_assets', 'copy:build_vendor_assets',
     'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
     'karma:continuous' 
   ]);
